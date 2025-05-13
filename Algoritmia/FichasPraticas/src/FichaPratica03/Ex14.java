b03facb62198b5e3e84809e8b23fdfde6cd4c74e
franciscoperez10 <fmlperez95@gmail.com>
2025-05-13 20:21:20 +0100

    Update Ex14.java

--- Algoritmia/FichasPraticas/src/FichaPratica03/Ex14.java
+++ Algoritmia/FichasPraticas/src/FichaPratica03/Ex14.java
@@ -1,5 +1,7 @@
 package FichaPratica03;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Ex14 {
@@ -7,32 +9,33 @@ public class Ex14 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int numeroAnterior = 0, numeroAtual = 0, contador = 0;int i = 1;
+        int numeroAnterior = 0, numeroAtual = 0, contador = 0, i = 1;
+        boolean flag = true;
 
-        System.out.println("Quantos números deseja inserir: ");
-        contador = input.nextInt();
-
-        if (contador <= 0) {
-            System.out.println("O número deve ser maior do que 0");
-        }
+        do {
+            System.out.println("Quantos números deseja inserir: ");
+            contador = input.nextInt();
+            if (contador <= 0) {
+                System.out.println("O número deve ser maior do que 0");
+                }
+        } while (contador <= 0);
 
         System.out.println("Introduza um número: ");
         numeroAnterior = input.nextInt();
 
-
-        while (i < contador) {
+        for (i=1; i < contador; i++) {
             System.out.println("Introduza o próximo número: ");
             numeroAtual = input.nextInt();
 
             if (numeroAtual < numeroAnterior) {
-                System.out.println("Ordem Não Crescente");
-            } else System.out.println("Ordem Crescente");
-
+                flag = false;
+            }
             numeroAnterior = numeroAtual;
-            i++;
-
-
         }
-
+        if (flag == true) {
+            System.out.println("Ordem Crescente");
+        } else {
+            System.out.println("Ordem Não Crescente");
+        }
     }
 }
