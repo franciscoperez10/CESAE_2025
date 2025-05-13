83c5d9a157271552398f878d7edea4b2e669214b
franciscoperez10 <fmlperez95@gmail.com>
2025-05-13 16:43:38 +0100

    Java Push

---
+++ ALG_PCE_POO/FichasPraticas/src/FichaPratica05/Ex07.java
@@ -0,0 +1,16 @@
+package FichaPratica05;
+
+import java.util.Scanner;
+
+public class Ex07 {
+    public static void main(String[] args) {
+
+        Scanner.input = new Scanner(System.in);
+
+        int[] vetor = new int[10];
+
+        for (int i = 1; i < vetor.length; i++) {
+
+        }
+    }
+}
