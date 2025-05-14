5c0dd04db35434c113347c335f5b543129e3d5a0
franciscoperez10 <fmlperez95@gmail.com>
2025-05-14 16:53:04 +0100

    Another one
    Funções

---
+++ ALG_PCE_POO/FichasPraticas/src/FichaPratica06/Ex01.java
@@ -0,0 +1,50 @@
+package FichaPratica06;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+    public static void somAnimais(String animal) {
+        switch (animal) {
+            case "cão":
+                System.out.println("Au au au");
+                break;
+            case "gato":
+                System.out.println("Miau");
+                break;
+            case "peixe":
+                System.out.println("glub glub");
+                break;
+            case "vaca":
+                System.out.println("muuu");
+                break;
+            case "porco":
+                System.out.println("oinc oinc");
+                break;
+            default:
+                System.out.println("Som não reconhecido, tente novamente");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner.input = new Scanner(System.in);
+
+
+        System.out.println("O cão faz: ");
+        somAnimais = (animal);
+
+        System.out.println("O gato faz: ");
+        somAnimais(somAnimais();
+
+        System.out.println("O peixe faz: ");
+        somAnimais(somAnimais();
+
+        System.out.println("A vaca faz: ");
+        somAnimais(somAnimais();;
+
+        System.out.println("O porco faz: ");
+        somAnimais(somAnimais();
+    }
+}
