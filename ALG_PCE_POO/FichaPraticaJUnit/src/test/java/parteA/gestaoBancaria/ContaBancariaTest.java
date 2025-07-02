c562c8cd9127598e4fbce1ac2124dc721d9fae1c
franciscoperez10 <fmlperez95@gmail.com>
2025-07-02 16:56:43 +0100

    Anotha One

---
+++ ALG_PCE_POO/FichaPraticaJUnit/src/test/java/parteA/gestaoBancaria/ContaBancariaTest.java
@@ -0,0 +1,91 @@
+package parteA.gestaoBancaria;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContaBancariaTest {
+
+    ContaBancaria conta1;
+    ContaBancaria conta2;
+
+
+    @BeforeEach
+    void setUp() {
+
+        conta1 = new ContaBancaria("FP", 100, 2025, "Portugal", "PT50 0000 0023 0000 0023");
+        conta2 = new ContaBancaria("TP", 5000, 2023, "Espanha", "ES20 0000 0025 0000 0000");
+    }
+
+    @Test
+    void testDepositar() {
+        assertEquals(100, conta1.getSaldo());
+        conta1.depositar(50);
+        assertEquals(150, conta1.getSaldo());
+
+        // controlo
+        assertEquals(5000, conta2.getSaldo());
+        conta2.depositar(1000);
+        assertEquals(6000, conta2.getSaldo());
+
+    }
+
+    @Test
+    void testLevantarValorMenorSaldo() {
+        assertEquals(100, conta1.consultarSaldo());
+        conta1.levantar(10);
+        assertEquals(90, conta1.consultarSaldo());
+
+    }
+
+    @Test
+    void testLevantarValorIgualSaldo() {
+        assertEquals(100, conta1.consultarSaldo());
+        conta1.levantar(100);
+        assertEquals(0, conta1.consultarSaldo());
+
+    }
+
+    @Test
+    void testLevantarValorMaiorSaldo() {
+        assertEquals(100, conta1.consultarSaldo());
+        conta1.levantar(300);
+        assertEquals(400, conta1.consultarSaldo());
+
+    }
+
+    @Test
+    void testLevantarValorInvalido() {
+        assertThrows(IllegalAccessException.class, () -> conta1.levantar(450000));
+        assertThrows(IllegalAccessException.class, () -> conta1.levantar(6000000));
+    }
+
+    @Test
+    void testTransferirValorMenor() {
+        assertEquals(1000, conta1.consultarSaldo());
+        conta1.transferir(conta2, 300);
+        assertEquals(2100), conta2;
+
+    }
+
+    @Test
+    void testTransferirValorIgual() {
+
+    }
+
+    @Test
+    void testTransferirValorMaior() {
+
+    }
+
+    @Test
+    void aplicarRendimentoMensal() {
+
+    }
+
+    @Test
+    void consultarSaldo() {
+
+    }
+}
